a43f6f56a6739d2190e38dc95ef1ba0b51527842
aCeTotal <lars.oksendal@gmail.com>
2026-08-17 00:30:58 +0200

    Changes Applications

--- modules/core/gpu/detected.nix
+++ modules/core/gpu/detected.nix
@@ -3,7 +3,7 @@
 # hardware.nvidia.prime.*BusId krever. Tom streng = ingen slik GPU.
 {
   nvidia = "PCI:1@0:0:0";
-  intel = "PCI:0@0:2:0";
+  intel = "";
   amd = "";
 
   # NVIDIA-arkitektur fra PCI-device-ID, og driver-branchen den trenger.
