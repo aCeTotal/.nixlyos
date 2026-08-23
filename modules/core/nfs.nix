156444889f0a510e48d75bf246ad2df4247e025d
aCeTotal <lars.oksendal@gmail.com>
2026-08-23 19:20:22 +0200

    Changes Applications

--- modules/core/nfs.nix
+++ modules/core/nfs.nix
@@ -1,5 +1,26 @@
 { lib, pkgs, ... }:
 
+let
+  nfsOptions = lib.concatStringsSep "," [
+    "rw"
+    "vers=4.2"
+    "rsize=1048576"
+    "wsize=1048576"
+    "nconnect=8"
+    "soft"
+    "timeo=5"
+    "retrans=2"
+    "retry=0"
+    "fsc"
+    "acl"
+    "noatime"
+    "nodiratime"
+    "tcp"
+    "lookupcache=all"
+    "actimeo=300"
+    "nocto"
+  ];
+in
 {
   # NFS support.
   boot.supportedFilesystems = [ "nfs" ];
@@ -25,6 +46,7 @@
   systemd.tmpfiles.rules = [
     "d /mnt/nfs 0755 root root -"
     "d /mnt/nfs/Bigdisk1 0755 root root -"
+    "d /mnt/nfs/Bigdisk2 0755 root root -"
   ];
 
   # Fast reachability check that fails in about a second.
@@ -40,47 +62,55 @@
     };
   };
 
-  # Mount unit gated on the reachability check above.
-  systemd.mounts = [{
-    what = "10.0.0.8:/bigdisk1";
-    where = "/mnt/nfs/Bigdisk1";
-    type = "nfs";
-    mountConfig = {
-      Options = lib.concatStringsSep "," [
-        "rw"
-        "vers=4.2"
-        "rsize=1048576"
-        "wsize=1048576"
-        "nconnect=8"
-        "soft"
-        "timeo=5"
-        "retrans=2"
-        "retry=0"
-        "fsc"
-        "acl"
-        "noatime"
-        "nodiratime"
-        "tcp"
-        "lookupcache=all"
-        "actimeo=300"
-        "nocto"
-      ];
-      TimeoutSec = "5s";
-    };
-    requires = [ "nfs-bigdisk1-check.service" ];
-    after = [ "nfs-bigdisk1-check.service" "network-online.target" ];
-    wants = [ "network-online.target" ];
-    unitConfig = {
-      StartLimitIntervalSec = 0;
-    };
-  }];
+  # Mount units gated on the reachability check above.
+  systemd.mounts = [
+    {
+      what = "10.0.0.8:/bigdisk1";
+      where = "/mnt/nfs/Bigdisk1";
+      type = "nfs";
+      mountConfig = {
+        Options = nfsOptions;
+        TimeoutSec = "5s";
+      };
+      requires = [ "nfs-bigdisk1-check.service" ];
+      after = [ "nfs-bigdisk1-check.service" "network-online.target" ];
+      wants = [ "network-online.target" ];
+      unitConfig = {
+        StartLimitIntervalSec = 0;
+      };
+    }
+    {
+      what = "10.0.0.8:/bigdisk2";
+      where = "/mnt/nfs/Bigdisk2";
+      type = "nfs";
+      mountConfig = {
+        Options = nfsOptions;
+        TimeoutSec = "5s";
+      };
+      requires = [ "nfs-bigdisk1-check.service" ];
+      after = [ "nfs-bigdisk1-check.service" "network-online.target" ];
+      wants = [ "network-online.target" ];
+      unitConfig = {
+        StartLimitIntervalSec = 0;
+      };
+    }
+  ];
 
-  # Automount triggers on access.
-  systemd.automounts = [{
-    where = "/mnt/nfs/Bigdisk1";
-    automountConfig = {
-      TimeoutIdleSec = "2min";
-    };
-    wantedBy = [ "multi-user.target" ];
-  }];
+  # Automounts trigger on access.
+  systemd.automounts = [
+    {
+      where = "/mnt/nfs/Bigdisk1";
+      automountConfig = {
+        TimeoutIdleSec = "2min";
+      };
+      wantedBy = [ "multi-user.target" ];
+    }
+    {
+      where = "/mnt/nfs/Bigdisk2";
+      automountConfig = {
+        TimeoutIdleSec = "2min";
+      };
+      wantedBy = [ "multi-user.target" ];
+    }
+  ];
 }
